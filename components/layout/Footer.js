84f47d6202d50997ec04ab0508e198826b800070
Serhio15210 <79569048+Serhio15210@users.noreply.github.com>
2022-03-27 14:15:12 +0300

    Update Footer.js

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 
 import { makeStyles } from "@material-ui/core/styles";
 
-import { Container, Grid, Typography } from "@material-ui/core";
+ 
 
 import { routes } from "data/routes";
 import Social from "components/Social";
